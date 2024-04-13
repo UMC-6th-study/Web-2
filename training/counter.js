54262a7b587f9d24070b095821a3e1213a9153f1
ssxrxbx <123061283+ssxrxbx@users.noreply.github.com>
2024-04-13 18:47:04 +0900

    counter
    training counter

---
+++ training/counter.js
@@ -0,0 +1,16 @@
+// import "./style.css";
+
+const number = document.getElementById("number");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+
+increase.onclick = () => {
+    const current = parseInt(number.innerText, 10);
+    number.innerText = current + 1;
+};
+
+decrease.onclick = () => {
+    const current = parseInt(number.innerText, 10);
+    number.innerText = current - 1;
+};
+
